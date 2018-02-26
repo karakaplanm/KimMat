9276137c2554d1bbb279edeb6055a83ab175b363
karakaplanm <karakaplanm@gmail.com>
2018-02-26 15:30:34 +0300

    Ekleme

---
+++ kokbul.py
@@ -0,0 +1,13 @@
+import math as m
+a=3
+b=2
+c=-3
+d=b**2-4*a*c
+if d>=0:
+  x1=(-b+m.sqrt(d))/(2*a)
+  x2=(-b-m.sqrt(d))/(2*a)
+  print('x1=',x1)
+  print('x2=',x2)
+	
+else:
+  print('Gercel kok yok')
---
+++ polinomkok.py
@@ -0,0 +1,3 @@
+import numpy as np
+a=np.roots([3,2,2,-1])
+print (a)
